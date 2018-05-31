52977f07c0432f9c6268bbcfd762527fe30b0a72
Patrick Fitzgerald <pfitzgerald@pch.com>
2018-05-30 23:36:06 -0400

    users table migration

--- database/migrations/2018_05_31_031132_create_users_table.php
+++ database/migrations/2018_05_31_031132_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('fname', 30);
+            $table->string('lname', 30);
+            $table->string('email', 30);
+            $table->string('password', 50);
+            $table->boolean('admin');
+            $table->boolean('paid');
             $table->timestamps();
         });
     }
