a43c5c48432c507430bc50b33640df3837369d87
Patrick Fitzgerald <pfitzgerald@pch.com>
2018-06-13 23:22:28 -0400

    added icon

--- resources/views/partials/errors.blade.php
+++ resources/views/partials/errors.blade.php
@@ -1,7 +1,7 @@
 @if ($errors->any())
     <div class="card card-outline-secondary">
         <div class="card-header bg-danger">
-            <h3 class="text-white">Errors</h3>
+            <h5 class="text-white"><i class="fas fa-exclamation-triangle"></i> Errors</h5>
         </div>
         <div class="card-body">
             @foreach ($errors->all() as $error)
