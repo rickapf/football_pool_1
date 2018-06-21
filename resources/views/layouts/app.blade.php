058dbc7a472962005f665b811db6a99f88150b6a
Patrick Fitzgerald <pfitzgerald@pch.com>
2018-06-21 11:34:20 -0400

    added icons to buttons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <title>Football Pool</title>
 </head>
@@ -18,14 +18,14 @@
         </div>
         <h4 class="p-1 text-primary font-weight-bold">Thomas Fitzgerald Sr. Memorial Football Pool</h4>
         @if (Auth::user())
-            <div class="text-center" role="groupx">
-                <button type="button" class="btn btn-sm btn-outline-primary">HOME</button>
-                <button type="button" class="btn btn-sm btn-outline-primary">RULES/INFO</button>
-                <button type="button" class="btn btn-sm btn-outline-primary">PICKS</button>
-                <button type="button" class="btn btn-sm btn-outline-primary">STANDINGS</button>
-                <button type="button" class="btn btn-sm btn-outline-primary">FEEDBACK</button>
-                <button type="button" class="btn btn-sm btn-outline-primary">ADMIN</button>
-                <button type="button" class="btn btn-sm btn-outline-primary">LOGOUT</button>
+            <div class="text-center">
+                <a class="btn btn-sm btn-outline-primary mr-1" role="button" href="{{route('home')}}"><i class="fas fa-home"></i> HOME</a>
+                <a class="btn btn-sm btn-outline-primary mr-1" role="button" href=""><i class="fas fa-info-circle"></i> RULES/INFO</a>
+                <a class="btn btn-sm btn-outline-primary mr-1" role="button" href=""><i class="fas fa-pencil-alt"></i> PICKS</a>
+                <a class="btn btn-sm btn-outline-primary mr-1" role="button" href=""><i class="fas fa-list-ol"></i> STANDINGS</a>
+                <a class="btn btn-sm btn-outline-primary mr-1" role="button" href=""><i class="fas fa-comment"></i> FEEDBACK</a>
+                <a class="btn btn-sm btn-outline-primary mr-1" role="button" href=""><i class="fas fa-wrench"></i> ADMIN</a>
+                <a class="btn btn-sm btn-outline-primary text-danger" role="button" href="{{route('logout')}}"><i class="fas fa-sign-out-alt"></i> LOGOUT</a>
             </div>
         @endif
     </div>
